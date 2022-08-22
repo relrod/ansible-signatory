dc41a78496a95bde2aaa17ed74bf5fa6ec4ad0ac
relrod <rick@elrod.me>
2022-08-22 18:28:45 +0000

    Deploying to gh-pages from @ ansible/ansible-sign@c46035d948a52dc7e0921e8858d2a73ae7de604e 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev1+gff1e6a5',
+    VERSION: '0.0.post1.dev1+gc46035d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
