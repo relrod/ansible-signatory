3ff1198a2c74502f6e3ad08b5962369aa1f2dd3e
shanemcd <me@shanemcd.com>
2022-08-22 15:06:46 +0000

    Deploying to gh-pages from @ ansible/ansible-sign@b68c6dfa1819938a93dcea156c98a0981ea0adc2 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev1+g9327929',
+    VERSION: '0.0.post1.dev1+gb68c6df',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
